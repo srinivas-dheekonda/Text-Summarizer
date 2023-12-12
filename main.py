20468c692857e4e16aa04902a6df3d1caa267994
Srinivasara_dheekonda <vasuvasu.srinu1996@gmail.com>
2023-12-13 00:03:02 +0530

    requirements & installation done

---
+++ main.py
@@ -0,0 +1,4 @@
+from textsummerizer.logging import logger
+
+
+logger.info("welcome to our custom ")
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r",encoding="utf-8") as f:
 __version__ ="0.0.0"
 REPO_NAME = "Text_summarization"
 AUTHOR_USERNAME ="srirao"
-SRC_REPO = "Textsummarizer"
+SRC_REPO = "textsummarizer"
 AUTHOR_EMAIL = "vasuvasu.srinu1996@gmail.com"
 
 
